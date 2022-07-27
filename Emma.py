16be065fd31f9d40524a26fcadd4681e3e93be21
Emma Horner <emmahorner@Emmas-MacBook-Pro-4.local>
2022-07-27 16:29:40 -0700

    added function 'sterile_production' that calculates \omega_h^2, plots the distribution, and saves the data

--- Emma.py
+++ Emma.py
@@ -1,8 +1,12 @@
 import numpy as np
 import numba as nb
+import matplotlib.pyplot as plt
 import run
 from Francisco import gstar
 import der
+import ODEsolver as solve
+from run import g, temp, gs, gss
+from run import with_spline_ODE, steps_taken
 
 G_F = 1.1663787e-5*(1/1000**2)
 m_pc = 1.22e22
@@ -88,3 +92,61 @@ def e_density(mass_s, eps, fe, anti_fe):
     c = 8*np.pi/(3*m_pc**2)*((1/2.13e-39)**2)*mass_s/(2*np.pi**2)*T_cm03
     oh2 = c*(der.trapezoid(eps, fe) + der.trapezoid(eps, anti_fe))
     return oh2
+
+def sterile_production(N, flavor, mass_s, mixangle_vacuum, L_0, make_plot=True): 
+    index = np.where(temp < 1/2000)[0][-1]
+    index2 = np.where(temp < 1/10)[0][-1]
+    x0 = temp[index]
+    f_initial = np.zeros(N)
+    f_anti_intial = np.zeros(N)
+    y_0 = np.zeros(2*N+3)
+    p = np.zeros(N+18)
+    
+    x0 = temp[index] 
+    dx0 = x0 / 1000
+    xf = temp[index + 1]
+    
+    y_0[:N] = f_initial
+    y_0[N:2*N] = f_anti_intial
+    y_0[-3:] = [L_0, 0, x0]
+    p[:N] = np.linspace(10/N, 10, N)
+    p[-5:] = [L_0, L_0, L_0, mixangle_vacuum, mass_s]
+    p[-10:-5] = gs[index, :] 
+    p[-15:-10] = gss[index, :]
+    flavor_vals = {"electron":[0,1.27,0.511], "muon":[1,1.27,105.658], "tau":[2,0.92,1777]}
+    p[-18:-15] = flavor_vals[flavor]
+    x, y , dx, s = steps_taken(x0, y_0, dx0, p, xf, index, index2)
+    
+    mixing_angle  = str(p[-2])
+    lepton_number = str(p[-3])
+
+    np.savez(flavor + 'x' + lepton_number +'x'+ mixing_angle , 
+             T = 1/x, 
+             final_distribution = y[-1,:N] + y[-1,N:2*N], 
+             epsilon = p[:N], 
+             lep_evo = y[:,-3],
+             a = y[:,-1], 
+             t = y[:,-2], 
+             sterile_evo = y[:,:N],  
+             anti_sterile_evo = y[:,N:2*N],  
+             mass_s = p[-1],
+             mixangle_vacuum = p[-2], 
+             initial_electron = p[-3],
+             initial_mu = p[-4],
+             initial_tau = p[-5],
+             m = p[-16], #mass of charged lepton
+             scattering_constant = p[-17],
+             flavor_int = p[-18], #integer to specify flavor
+             omega_h2 = e_density(p[-1], p[:N], p[:N]**2*y[-1,:N], p[:N]**2*y[-1,N:2*N]))
+
+    if make_plot:
+        plt.figure()
+        plt.plot(p[:N], p[:N]**2*y[-1,:N])
+        plt.plot(p[:N], p[:N]**2*y[-1,N:2*N])
+        plt.plot(p[:N], p[:N]**2*y[-1,:N]+p[:N]**2*y[-1,N:2*N], '--')
+        plt.xlabel(r'$\epsilon$')
+        plt.ylabel(r"$\epsilon^2 f$")
+        
+    omega_h2 = e_density(p[-1], p[:N], p[:N]**2*y[-1,:N], p[:N]**2*y[-1,N:2*N])
+    
+    return omega_h2 
