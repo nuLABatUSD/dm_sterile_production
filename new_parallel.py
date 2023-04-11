8446ae265fa35e2135e45190e6f7145c4aa397f8
nuLABatUSD <ckishimoto@sandiego.edu>
2023-04-11 11:22:07 -0700

    ready to run x5

--- new_parallel.py
+++ new_parallel.py
@@ -3,14 +3,18 @@ import multiprocessing as mp
 import time
 import Num_SH as NSH
 
+Run_index = 0
+folder = ["ThreeEqual", "E_Mu", "Tau", "AllTau", "AllMu"]
+file_header = ["three_equal", "mostly_emu", "mostly_tau", "all_tau", "all_mu"]
+
+mult_factor = [[1./3, 1./3, 1./3], [10./21, 10./21, 1./21], [1./12, 1./12, 10./12], [0, 0, 1], [0, 1, 0]]
 
 def parallel_equal(mix,lep):
-    return NSH.solve(mix/3,mix/3,mix/3,lep,lep,lep, "Quick", "three_equal", make_plot=False)
+    return NSH.solve(mix * mult_factor[Run_index][0],mix * mult_factor[Run_index][1],mix * mult_factor[Run_index][2],lep,lep,lep, folder[Run_index], file_header[Run_index], make_plot=False)
     
 if __name__ == '__main__':
-    mixang = np.linspace( 1e-10, 3e-9, 10) #use 19
-    lep0 = np.linspace(1e-3, 9e-3, 9) #use 19
-    
+    mixang = np.linspace( 1e-10, 3e-9, 30)
+    lep0 = np.linspace(1e-3, 7e-3, 31)     
     run_list = []
     new_list = []
     for i in range(len(mixang)):
@@ -29,6 +33,6 @@ if __name__ == '__main__':
     print("Parallel, elapsed time = {} seconds".format(time.time()-new_start_time))
     #print(res)
     
-    np.savez("DataRun/three_equal-results", results = res, mixangle = mixang, L0 = lep0, index = new_list)
+    np.savez("{}/{}-results".format(folder[Run_index], file_header[Run_index]), results = res, mixangle = mixang, L0 = lep0, index = new_list)
     
 
